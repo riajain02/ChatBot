b47fd1b00d1faaa8d78fe9031e0ecb1ee4937660
ria <riajain3878@gmail.com>
2021-03-08 09:09:59 -0800

    update to Bot.java

--- Bot.java
+++ Bot.java
@@ -5,15 +5,29 @@ public class Bot
     public static void main(String args[])
     {
         System.out.println("\nWelcome to ChatBot!");
-        System.out.println("Ask me anything by typing below. Type 'QUIT' to end the program.\n");
+        System.out.println("Ask me anything by typing below. Type \"QUIT\" to end the program.\n");
         Scanner sc = new Scanner(System.in);
-        System.out.print("Ask ChatBot: ");
+        System.out.print("You: ");
         String input = sc.nextLine();
         while(!input.equals("QUIT"))
         {
-            System.out.print("Ask ChatBot: ");
+            System.out.println("Bot: " + response(input));
+            System.out.print("You: ");
             input = sc.nextLine();
         }
-        System.out.println("\nChatbot program has been quit.\n");
+        System.out.println("\nGoodbye.");
+        System.out.println("Chatbot program has ended.\n");
+    }
+    
+    private static String response(String text)
+    {
+        if(text.charAt(text.length()-1) == '?') // input is a question
+        {
+            return "y";
+        }
+        else // input is a statement or exclamation
+        {
+            return "x";
+        }
     }
 }
